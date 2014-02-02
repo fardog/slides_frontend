8d5969066cf6cebbd64ac294eee6be96412de817
nwittstock <nwittstock@north.com>
2014-02-01 18:34:49 -0800

    Better comments, and simplify presentation start

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
-var $media = null;
+// money variables in the global scope, since we use them in our KO data-binds
+var $media = null,
+		$prefix = null,
+		$base = null;
 
 $(document).ready(function() {
 
-	// initialize Foundation
+	// initialize Zurb Foundation
 	$(document).foundation();
 
-	var $prefix = $("meta[property='slides:api']").attr("content");
-	var $base = document.baseURI;
+	// we store some variable properties in the HTML, get them.
 	$media = $("meta[property='slides:media']").attr("content");
+	$prefix = $("meta[property='slides:api']").attr("content");
+	$base = document.baseURI;
 
 	/***
 	 * viewModel: an our bound object for KnockoutJS
@@ -20,17 +24,24 @@ $(document).ready(function() {
 			$.get($prefix + url, params, function(data, textStatus, jqXHR) {
 				next(data);
 			}).fail(function (data) {
+				// TODO handle failures better
 				console.log("Failed to load data.");
 				console.log(data);
 			});
 		};
 
 
+		// list of presentations that were loaded from the server
 		self.presentations = ko.observableArray();
-		self.assets = ko.observableArray();
+		// list of assets (slides) in the currently running presentation
+		self.assets = ko.observableArray([]);
+		// the currently displayed slide
 		self.currentSlide = ko.observable();
+		// whether or not the slideshow is running
 		self.slideshowRunning = ko.observable(null);
+		// the current window height in pixels
 		self.windowHeight = ko.observable($(window).height() + "px");
+		// the timer that advances to the next slide
 		self.timeout = null;
 
 		/***
@@ -43,27 +54,29 @@ $(document).ready(function() {
 		 * returns nothing
 		 */
 		self.loadPresentation = function(data, event) {
-			// set visibility on each slide
-			data.assets[0].visible = ko.observable(true);
-			for (var i=1; i<data.assets.length; i++) {
+			// put a visibility marker on each slide
+			for (var i=0; i<data.assets.length; i++) {
 				data.assets[i].visible = ko.observable(false);
 			}
 			self.assets(data.assets);
 
-			// set the current slide
-			self.currentSlide(0);
+			// set the current slide to -1, so we fade in the first slide
+			self.currentSlide(-1);
 
 			// if there's a slideshow running, stop it
 			self.stopPresentation();
 
-			// start the next slideshow
+			// mark the slideshow as running. bail if there's one running already
 			if (self.timeout || self.slideshowRunning()) return;
 			self.slideshowRunning(true);
-			$('body').css('cursor', 'none'); // hide cursor
-			self.timeout = setTimeout(function() {
-				self.advanceSlide();
-			}, self.assets()[self.currentSlide()].time * 1000);
+
+			// hide the cursor
+			$('body').css('cursor', 'none');
+
+			// show the first slide
+			self.advanceSlide();
 		};
+
 
 		/***
 		 * stopPresentation: stops the currently running presentation
@@ -71,14 +84,20 @@ $(document).ready(function() {
 		 * returns nothing
 		 */
 		self.stopPresentation = function() {
-			if (self.slideshowRunning()) {
-				self.slideshowRunning(false);
-				$('body').css('cursor', 'auto'); // show cursor again
-				self.assets([]);
-			}
+			// clear the timeout
 			clearTimeout(self.timeout);
 			self.timeout = null;
+
+			// if the slideshow is running, we reset things to non-running state
+			if (self.slideshowRunning()) {
+				self.slideshowRunning(false);
+				// show the cursor again
+				$('body').css('cursor', 'auto');
+				// blank out the asset list
+				self.assets([]);
+			}
 		};
+
 
 		/***
 		 * advanceSlide: advances the currently running slideshow to the next slide
@@ -86,17 +105,26 @@ $(document).ready(function() {
 		 * returns nothing
 		 */
 		self.advanceSlide = function() {
-			var length = self.assets().length;
+			// the next slide should be one after the current
 			var nextSlide = self.currentSlide() + 1;
-			if (nextSlide >= length) nextSlide = 0;
-			self.assets()[self.currentSlide()].visible(false);
+			// if we've overrun the end of the assets though, go back to the beginning
+			if (nextSlide >= self.assets().length) nextSlide = 0;
+
+			// special case for the first slide, since we set -1 above
+			if (self.currentSlide() >= 0)
+				self.assets()[self.currentSlide()].visible(false);
+
+			// make the next slide visible, and set it as current slide
 			self.assets()[nextSlide].visible(true);
 			self.currentSlide(nextSlide);
+
+			// set timeout for the next advance
 			self.timeout = setTimeout(function() {
 				if (self.slideshowRunning()) self.advanceSlide();
 			}, self.assets()[self.currentSlide()].time * 1000);
 		};
 	}; /* end viewModel */
+
 
 	// create a binding handler for a fade transition
 	ko.bindingHandlers.visibleFade = {
